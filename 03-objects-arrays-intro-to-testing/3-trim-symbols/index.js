33a24317b0f218acf1203e318f24c632b1b0f9ca
melnik <melnik@bifit.com>
2022-01-26 16:07:20 +0300

    Solutions for 03-objects-arrays-intro-to-testing

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,23 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  const stringList = string.split('');
+  let counter = 1;
+  let prevLetter = "";
 
+  if (size === 0) {
+    return '';
+  }
+
+  return stringList.reduce((acc, current) => {
+    if (current !== prevLetter) {
+      counter = 1;
+      prevLetter = current;
+      acc.push(current);
+    } else if (counter !== size) {
+      counter++;
+      acc.push(current);
+    }
+    return acc;
+  }, []).join('');
 }
